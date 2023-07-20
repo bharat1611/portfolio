910f7e23321e46d0b5a9d80cd08df08bba63f345
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2023-07-20 23:05:46 +0530

    About Section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assests/me2.jpg'
 
 const About = () => {
   return (
     <section id = "about">
-      About
+      <h5>Get to Know</h5>
+      <h2>About me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+            <div className = "about__me-image">
+              <img src = {ME} alt = "About Image"/>
+
+            </div>
+        </div>
+        <div className='about_content'>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat aliquam pariatur explicabo nulla quia, dignissimos id hic mollitia officiis animi quo provident quos sint quisquam corporis eveniet asperiores consequuntur dolores!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum nostrum nisi, aliquam dolorem, possimus ea suscipit optio quisquam dolore quod cupiditate molestiae earum repellat doloremque non fugit cum eligendi atque!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae, rem tenetur! Accusamus nobis harum voluptatum error saepe perspiciatis eos consectetur animi provident labore facere magni, doloremque ex exercitationem, officiis veniam?
+          </p>
+          <a href="#contact" className= 'btn btn-primary'>Contact Me</a>
+        </div>
+      </div>
     </section>
   )
 }
