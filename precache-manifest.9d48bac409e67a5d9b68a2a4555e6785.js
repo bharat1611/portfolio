5dd8bc3c273378e1070cdb64e0de7bf2a3d4d020
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2023-07-23 23:28:45 +0530

    Updates

---
+++ precache-manifest.9d48bac409e67a5d9b68a2a4555e6785.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b90d1fa32fed89c9154",
+    "url": "/portfolio/static/css/main.39965ea2.chunk.css"
+  },
+  {
+    "revision": "5b90d1fa32fed89c9154",
+    "url": "/portfolio/static/js/main.54ada94b.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "c45ee193ce756cbfdcc6",
+    "url": "/portfolio/static/js/2.8b522c6b.chunk.js"
+  },
+  {
+    "revision": "77753132427ad608a4740e19f23d668f",
+    "url": "/portfolio/static/media/My_Resume___ML.77753132.pdf"
+  },
+  {
+    "revision": "436fdb3be89ad6a508716ac3df2b4802",
+    "url": "/portfolio/static/media/me4.436fdb3b.jpg"
+  },
+  {
+    "revision": "6be635cd6aef4f38757aa31761b54f5a",
+    "url": "/portfolio/static/media/me10.6be635cd.jpg"
+  },
+  {
+    "revision": "d35b218b7adc4cb3cfbdbaa75db73fc3",
+    "url": "/portfolio/static/media/proj1.d35b218b.png"
+  },
+  {
+    "revision": "ac8ac21eba765c41935b7cbc883afc0b",
+    "url": "/portfolio/static/media/proj2.ac8ac21e.png"
+  },
+  {
+    "revision": "706ed8b1ef73c1e0a5bf21fad81add61",
+    "url": "/portfolio/static/media/proj3.706ed8b1.jpg"
+  },
+  {
+    "revision": "f67c1afbabbb3525caf4a3a6b3a36d1d",
+    "url": "/portfolio/static/media/proj4.f67c1afb.jpeg"
+  },
+  {
+    "revision": "feed429c1c5556af441fc7ee557aa22c",
+    "url": "/portfolio/static/media/proj5.feed429c.png"
+  },
+  {
+    "revision": "892bf45f76d3e0cfd15549989dde3e4c",
+    "url": "/portfolio/index.html"
+  }
+];
