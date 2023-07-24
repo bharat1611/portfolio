8988d3afdf4f62747e315f7b0d7840638fc2cc06
bharat1611 <upadhyay.bharat.2017641@gmail.com>
2023-07-24 12:26:30 +0530

    Adding changes to contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -14,9 +14,17 @@ const Contact = () => {
     emailjs.sendForm('service_i89jr4n', 'template_10hkrx8', form.current, 'sur4LbmCa8ZhnWMpf')
       .then((result) => {
           console.log(result.text);
+          messagesent();
       }, (error) => {
           console.log(error.text);
       });
+
+      const messagesent = () => {
+        alert("Got your message, will get back to you ASAP !");
+        document.getElementById("1").value = '';
+        document.getElementById("2").value = '';
+        document.getElementById("3").value = '';
+      }
   };
   return (
     <section id = "contact"> 
@@ -48,9 +56,9 @@ const Contact = () => {
       </div>
       {/* End of options */}
       <form ref = {form} onSubmit={sendEmail}>
-        <input type="text" name = 'name' placeholder='Your Full Name' required/>
-        <input type = "email" name = 'email' placeholder='Your Email' required/>
-        <textarea name = "message" rows = "7" placeholder='Your Message' required/>
+        <input type="text" name = 'name' id = "1" placeholder='Your Full Name' required/>
+        <input type = "email" name = 'email' id = "2" placeholder='Your Email' required/>
+        <textarea name = "message" rows = "7" id = "3" placeholder='Your Message' required/>
         <button type = 'submit' className = 'btn btn-primary'>Send Message</button>
       </form>
     </div>
